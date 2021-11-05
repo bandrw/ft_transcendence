240f44a3cacc860ab13bc1b3bfc93086a5ccdd20
bandrw <andr.bal@icloud.com>
2021-11-05 18:36:01 +0300

    db connection fix

--- nest-connector/app/src/app.module.ts
+++ nest-connector/app/src/app.module.ts
@@ -17,11 +17,11 @@ import { UsersModule } from 'users/users.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost', // todo [change host to 'db']
+      host: process.env.DB_HOST,
       port: 5432,
-      username: 'pfile',
-      password: 'pfile',
-      database: 'pfile',
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [User, GameEntity, UserSubscription],
     }),
     UsersModule,
