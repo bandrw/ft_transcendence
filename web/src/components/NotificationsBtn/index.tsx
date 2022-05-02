612525bbbd4cefd504809e6c06101f49de022c4c
credo7 <ah.vit@mail.ru>
2022-05-03 02:05:13 +0300

    fix / disable Notification-btn

---
+++ web/src/components/NotificationsBtn/index.tsx
@@ -0,0 +1,17 @@
+import './styles.scss';
+
+import { faBell } from '@fortawesome/free-solid-svg-icons/faBell';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export const NotificationsBtn = () => {
+	const showAlert = () => {
+		// eslint-disable-next-line
+		alert('Comming soon...');
+	};
+
+	return (
+		<button onClick={showAlert} className="notifications-btn_disabled" >
+			<FontAwesomeIcon icon={faBell} />
+		</button>
+	);
+}
